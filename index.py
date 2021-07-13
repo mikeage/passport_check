394011ec287174d89e717f8217279d28555f7882
Mike Miller <github@mikeage.net>
2021-07-13 16:37:00 +0300

    Refactor v4 (use mechanicalsoup)

--- index.py
+++ index.py
@@ -19,6 +19,7 @@ def check_months(browser, site):
             form = browser.select_form()
             form.set_select({'nMonth': str(m)})
 
+
 def check_month(browser):
     found = False
 
@@ -26,30 +27,33 @@ def check_month(browser):
     # Find all the td elements. bgcolor #c0c0c0 is grey, date passed/no appt
     # bgcolor #ADD9F4 is blue, full
     # bgcolor #ffffc0 is available appt!!!
-    calendar = browser.page.find_all(id="Table3")[0]
-    c_re = re.compile('<td.*?(\"#.*?\")')
-    bgcolors = [b.upper().replace("'", "").replace('"', '') for b in c_re.findall(str(calendar))]
+    unavailable_colors = ["#ADD9F4", "#C0C0C0"]
 
-    # the number of tr elements
-    c_tr = re.compile('(<tr)')
-    trs = c_tr.findall(str(calendar))
-    num_trs = len(trs) - 1 # don't count header row
-    c_tr2 = re.compile('<tr bgcolor="#ffffc0">\n</tr>') # an empty tr?
-    empty_tr = len(c_tr2.findall(str(calendar))) == 1
-    if empty_tr:
-        num_trs = num_trs - 1
-    # if the number of bgcolors is not number of calendar rows by 7 days 
-    if len(bgcolors) != (num_trs * 7):
-        found = True
-        return found
+    calendar = browser.page.find(id="Table3")
 
-    unique = list(set(bgcolors))
-    for color in unique:
-        if color not in regular_colors:
-            found = True
+    for table_row in calendar.select("tr"):
+        cells = table_row.findAll('td')
+        for cell in cells:
+            bgcolor = cell.attrs.get('bgcolor', None)
+            if bgcolor and bgcolor.upper() not in unavailable_colors:
+                print("Found an appointment on %s" % cell.text)
+                found = True
     return found
 
-def lookup(browser):
+
+def lookup(browser):  # pylint: disable=too-many-locals
+    jerusalem_start = 'https://evisaforms.state.gov/acs/default.asp?postcode=JRS&appcode=1'
+    telaviv_start = 'https://evisaforms.state.gov/acs/default.asp?postcode=TLV&appcode=1'
+    appt_dir = 'https://evisaforms.state.gov/acs/'
+    jerusalem_service = 'chkservice'
+    jerusalem_service_value = '03'  # name is chkservice
+    jerusalem_read_instructions = 'chkbox01'  # name
+    jerusalem_read_instructions_value = 'on'
+    ta_service = 'chkservice'
+    ta_service_value = 'AA'
+    ta_read_instructions = 'chkbox01'  # name
+    ta_read_instructions_value = 'on'
+
     site = "Jerusalem"
     browser.open(jerusalem_start)
     input_field = browser.page.find_all('input')[0]
@@ -74,22 +78,11 @@ def lookup(browser):
     browser[ta_read_instructions] = ta_read_instructions_value
     check_months(browser, site)
 
-jerusalem_start = 'https://evisaforms.state.gov/acs/default.asp?postcode=JRS&appcode=1'
-telaviv_start = 'https://evisaforms.state.gov/acs/default.asp?postcode=TLV&appcode=1'
-appt_dir = 'https://evisaforms.state.gov/acs/'
-jerusalem_service = 'chkservice'
-jerusalem_service_value = '03' # name is chkservice
-jerusalem_read_instructions = 'chkbox01' # name
-jerusalem_read_instructions_value = 'on'
-ta_service = 'chkservice'
-ta_service_value = 'AA'
-ta_read_instructions = 'chkbox01' # name
-ta_read_instructions_value = 'on'
-regular_colors = ["#ADD9F4", "#C0C0C0"]
 
 def main():
     browser = mechanicalsoup.StatefulBrowser()
     while True:
         lookup(browser)
+
 
 main()
